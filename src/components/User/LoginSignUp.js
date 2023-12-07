31e6ec3c86abd28fa08d4443dca2c7d0300de04c
Jeshika-code <jeshikasapkota5@gmail.com>
2023-12-07 22:14:42 +0545

    cloudinary test

--- src/components/User/LoginSignUp.js
+++ src/components/User/LoginSignUp.js
@@ -1,12 +1,21 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState,useEffect } from "react";
 import Loader from "../component/Loader/Loader";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 import FaceIcon from "@material-ui/icons/Face";
 import "./LoginSignUpBox.css";
+import {useDispatch,useSelector} from "react-redux";
+import { clearErrors, login,register } from "../../actions/userActions";
+import { useAlert } from "react-alert";
 
-const LoginSignUp = () => {
+const LoginSignUp = ({history}) => {
+    const dispatch=useDispatch();
+    const navigate = useNavigate();
+    const alert = useAlert();
+    const { error, loading,isAuthenticated } = useSelector(
+        (state) => state.user
+      );
     const loginTab = useRef(null);
     const registerTab = useRef(null);
     const switcherTab = useRef(null);
@@ -20,8 +29,9 @@ const LoginSignUp = () => {
     const { name, email, password } = user;
     const [avatar, setAvatar] = useState("/profile.jpg");
     const [avatarPreview, setAvatarPreview] = useState("/profile.jpg");
-    const loginSubmit = () => {
-        console.log("form submitted");
+    const loginSubmit = (e) => {
+        e.preventDefault();
+        dispatch(login(loginEmail, loginPassword));
     };
     const registerSubmit = (e) => {
         e.preventDefault();
@@ -30,6 +40,7 @@ const LoginSignUp = () => {
         myForm.set("email", email);
         myForm.set("password", password);
         myForm.set("avatar", avatar);
+        dispatch(register(myForm))
     };
     const registerDataChange = (e) => {
         if (e.target.name === "avatar") {
@@ -47,6 +58,17 @@ const LoginSignUp = () => {
           setUser({ ...user, [e.target.name]: e.target.value });
         }
       };
+      useEffect(() => {
+        if (error) {
+          alert.error(error);
+          dispatch(clearErrors());
+        }
+        if (isAuthenticated) {
+           navigate("/account");
+          }
+       
+      }, [dispatch, error, alert,history,isAuthenticated]);
+    
     const switchTabs = (e, tab) => {
         if (tab === "login") {
             switcherTab.current.classList.add("shiftToNeutral");
@@ -62,112 +84,113 @@ const LoginSignUp = () => {
         }
     };
     return (
-        <>
-            <div className="LoginSignUpContainer">
-                <div className="LoginSignUpBox mt-20 ">
-                    <div>
-                        <div className=" logintoggle flex ">
-                            <p
-                                className="cursor-pointer w-full mt-2  hover:text-text-orange transition-all ml-5  lg:text-xl text-sm duration-500 ease-in "
-                                onClick={(e) => switchTabs(e, "login")}
-                            >
-                                LOGIN
-                            </p>
-                            <p
-                                className="cursor-pointer mt-2 hover:text-text-orange w-full lg:ml-32 ml-10 lg:text-xl text-sm transition-all duration-500 ease-in "
-                                onClick={(e) => switchTabs(e, "register")}
-                            >
-                                REGISTER
-                            </p>
-                        </div>
-                        <button
-                            className="bg-black border-none  "
-                            ref={switcherTab}
-                        ></button>
-                    </div>
-                    <form className="loginForm" ref={loginTab} onSubmit={loginSubmit}>
-                        <div className="loginEmail">
-                            <MailOutlineIcon className="mr-1" />
-                            <input
-                                type="email"
-                                placeholder="Email"
-                                required
-                                value={loginEmail}
-                                onChange={(e) => setLoginEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="loginPassword">
-                            <LockOpenIcon className="mr-1" />
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                required
-                                value={loginPassword}
-                                onChange={(e) => setLoginPassword(e.target.value)}
-                            />
-                        </div>
-                        <Link
-                            className="self-end text-black transition-all duration-500 ease-in hover:text-text-orange mr-2 text-sm"
-                            to="/password/forgot"
-                        >
-                            Forget Password?
-                        </Link>
-                        <input type="submit" value="Login" className="loginBtn" />
-                    </form>
-                    <form
-                        className="signUpForm"
-                        ref={registerTab}
-                        encType="multipart/form-data"
-                        onSubmit={registerSubmit}
-                    >
-                        <div className="signUpName">
-                            <FaceIcon className="m-1"/>
-                            <input
-                                type="text"
-                                placeholder="Name"
-                                required
-                                name="name"
-                                value={name}
-                                onChange={registerDataChange}
-                            />
-                        </div>
-                        <div className="signUpEmail">
-                            <MailOutlineIcon className="m-1" />
-                            <input
-                                type="email"
-                                placeholder="Email"
-                                required
-                                name="email"
-                                value={email}
-                                onChange={registerDataChange}
-                            />
-                        </div>
-                        <div className="signUpPassword">
-                            <LockOpenIcon className="m-1" />
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                required
-                                name="password"
-                                value={password}
-                                onChange={registerDataChange}
-                            />
-                        </div>
+     <>{loading?<Loader/>:
+     <>
+     <div className="LoginSignUpContainer">
+         <div className="LoginSignUpBox mt-20 ">
+             <div>
+                 <div className=" logintoggle flex ">
+                     <p
+                         className="cursor-pointer w-full mt-2  hover:text-text-orange transition-all ml-5  lg:text-xl text-sm duration-500 ease-in "
+                         onClick={(e) => switchTabs(e, "login")}
+                     >
+                         LOGIN
+                     </p>
+                     <p
+                         className="cursor-pointer mt-2 hover:text-text-orange w-full lg:ml-32 ml-10 lg:text-xl text-sm transition-all duration-500 ease-in "
+                         onClick={(e) => switchTabs(e, "register")}
+                     >
+                         REGISTER
+                     </p>
+                 </div>
+                 <button
+                     className="bg-black border-none  "
+                     ref={switcherTab}
+                 ></button>
+             </div>
+             <form className="loginForm" ref={loginTab} onSubmit={loginSubmit}>
+                 <div className="loginEmail">
+                     <MailOutlineIcon className="mr-1" />
+                     <input
+                         type="email"
+                         placeholder="Email"
+                         required
+                         value={loginEmail}
+                         onChange={(e) => setLoginEmail(e.target.value)}
+                     />
+                 </div>
+                 <div className="loginPassword">
+                     <LockOpenIcon className="mr-1" />
+                     <input
+                         type="password"
+                         placeholder="Password"
+                         required
+                         value={loginPassword}
+                         onChange={(e) => setLoginPassword(e.target.value)}
+                     />
+                 </div>
+                 <Link
+                     className="self-end text-black transition-all duration-500 ease-in hover:text-text-orange mr-2 text-sm"
+                     to="/password/forgot"
+                 >
+                     Forget Password?
+                 </Link>
+                 <input type="submit" value="Login" className="loginBtn" />
+             </form>
+             <form
+                 className="signUpForm"
+                 ref={registerTab}
+                 encType="multipart/form-data"
+                 onSubmit={registerSubmit}
+             >
+                 <div className="signUpName">
+                     <FaceIcon className="m-1"/>
+                     <input
+                         type="text"
+                         placeholder="Name"
+                         required
+                         name="name"
+                         value={name}
+                         onChange={registerDataChange}
+                     />
+                 </div>
+                 <div className="signUpEmail">
+                     <MailOutlineIcon className="m-1" />
+                     <input
+                         type="email"
+                         placeholder="Email"
+                         required
+                         name="email"
+                         value={email}
+                         onChange={registerDataChange}
+                     />
+                 </div>
+                 <div className="signUpPassword">
+                     <LockOpenIcon className="m-1" />
+                     <input
+                         type="password"
+                         placeholder="Password"
+                         required
+                         name="password"
+                         value={password}
+                         onChange={registerDataChange}
+                     />
+                 </div>
 
-                        <div id="registerImage">
-                            <img src={avatarPreview} alt="Avatar Preview" />
-                            <input
-                                type="file"
-                                name="avatar"
-                                accept="image/*"
-                                onChange={registerDataChange}
-                            />
-                        </div>
-                        <input type="submit" value="Register" className="signUpBtn " />
-                    </form>
-                </div>
-            </div>
-        </>
+                 <div id="registerImage">
+                     <img src={avatarPreview} alt="Avatar Preview" />
+                     <input
+                         type="file"
+                         name="avatar"
+                         accept="image/*"
+                         onChange={registerDataChange}
+                     />
+                 </div>
+                 <input type="submit" value="Register" className="signUpBtn " />
+             </form>
+         </div>
+     </div>
+ </>}</>
     );
 };
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+// import { configureStore } from '@reduxjs/toolkit'
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productDetailsReducer, productsReducer } from "./reducers/productReducer";
+import { userReducer } from "./reducers/userReducer";
 
 const reducer = combineReducers({
   products: productsReducer,
-  productDetails:productDetailsReducer
+  productDetails:productDetailsReducer,
+  user:userReducer,
+
 });
 let initialState = {}
 const middleware = [thunk];
